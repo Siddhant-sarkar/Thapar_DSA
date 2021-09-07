672002753681a603dd14f96e91cc5470a11d7365
Siddhant Sarkar <siddhant.sarkar999@gmail.com>
2021-09-07 11:47:37 +0530

    finally saved some more work

---
+++ matrixMultiplication.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class matrixMultiplication{
+	public static void print(int[][]arr){
+		for(int i=0;i<arr.length;i++){
+			for(int j=0;j<arr[0].length;j++){
+				System.out.print(arr[i][j]+" ");	
+			}
+			System.out.println();
+		}
+	}
+	public static void main(String[] args) {
+		int m1[][]={
+								{1,2,3},
+								{4,5,6},
+								{7,8,9}
+							};
+		int m2[][]={
+								{1,2,3},
+								{4,5,6},
+								{7,8,9}
+							};
+		int mlti[][]= new int[m1.length][m2[0].length];
+		for(int i=0;i<m1.length;i++){
+			for(int j=0;j<m2[0].length;j++){
+				for(int k=0;k<m2.length;k++) mlti[i][j]+=m1[i][k]*m2[k][j];
+			}
+		}
+		System.out.println("Matrix 1");
+		print(m1);
+		System.out.println("Matrix 2");
+		print(m2);
+		System.out.println("Matrix Multiplication : ");
+		print(mlti);
+	}
+}
