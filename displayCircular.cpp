5f7ca118aceff3bc5d426a712793016aa414354f
Siddhant Sarkar <siddhant.sarkar999@gmail.com>
2021-10-16 20:21:52 +0530

    some of the shit that i dont ever want to do

---
+++ displayCircular.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+public:
+	int data;
+	Node* next;
+};
+
+class LinkedList{
+private:
+	Node* head,* tail;
+public:
+	LinkedList(){
+		head=NULL;
+		tail=NULL;
+	}
+	void insertAtTail(int data){
+		Node* n = new Node;	
+		n->data=data;n->next=NULL;
+
+		if(head== NULL){head=n;tail=n;}
+		else{
+			tail->next=n;
+			tail=tail->next;
+		}
+		tail->next=head;
+	}
+	void display(){
+		if(head== NULL){
+			cout<<"The LinkedList is not initialized\n";
+		}else{
+			Node * n = head;
+			do{
+				cout<<n->data<<" ";
+				n=n->next;
+			}while(n!=head);
+			cout<<n->data<<endl;
+		}
+	}
+
+};
+int main(){
+	LinkedList ll;
+	ll.insertAtTail(1);
+	ll.insertAtTail(2);
+	ll.insertAtTail(3);
+	ll.insertAtTail(4);
+	ll.insertAtTail(5);
+	ll.display();
+}
--- one.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-#include<sys/types.h>
-#include<sys/stat.h>
-int main(){
-  int res;
-  res= mkfifo("fifo1",0777);
-  printf("named pipe character\n");
-}
